fba7dfa56a3b58c9481f75d84b270ad8b92d9b6b
Stav42 <vaditya@iitk.ac.in>
2021-05-27 00:20:13 +0530

    adding files

---
+++ calibration/cameraCalibration/calibrate.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<opencv2/calib3d.hpp>
+#include<opencv2/core.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+
+int main(int argc, char** argv){
+
+    (void)argc;
+    (void)argv;
+
+    std::vector<cv::String> fileNames;
+    cv::glob("/")
+
+    
+}
